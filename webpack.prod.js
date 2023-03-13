75a3bfaa4f8e56b0f2d0647152b0addafd914fb8
slimemoss <common.tken@gmail.com>
2023-03-13 21:24:01 +0900

    fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,6 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    publicPath: '/yugioh-search/#/',
+    publicPath: '/yugioh-search/',
   },
 })
